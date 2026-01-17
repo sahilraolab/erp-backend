ba5bcba93c59f89c13497c994419401b05154b9b
cbsahil <sahil@cueblocks.com>
2026-01-18 00:49:03 +0530

    Project clean up

--- src/modules/tax/tax.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth = require('../../core/auth.middleware');
-const report = require('./taxReport.service');
-
-router.get('/gst-summary', auth, report.gstSummary);
-router.get('/wct-summary', auth, report.wctSummary);
-
-module.exports = router;
